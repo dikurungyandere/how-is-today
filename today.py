84c85a03e7c81edc0b798ebe83a0c5fed4005aeb
Yana Rio <255517823+dikurungyandere@users.noreply.github.com>
2026-04-20 01:30:40 +0000

    Add -t/--tomorrow flag to get message for tomorrow

--- today.py
+++ today.py
@@ -68,6 +68,7 @@ def main():
     parser.add_argument("-c", "--count", type=int, default=1, help="Number of messages to show")
     parser.add_argument("-r", "--random", action="store_true", help="Get a random message (not seeded by date)")
     parser.add_argument("-d", "--date", type=str, help="Get message for date (YYYY-MM-DD)")
+    parser.add_argument("-t", "--tomorrow", action="store_true", help="Get message for tomorrow")
     parser.add_argument("-j", "--json", action="store_true", help="Output as JSON")
     parser.add_argument("-l", "--list", action="store_true", help="List all available messages")
     parser.add_argument("-o", "--output", type=str, help="Save message to file")
@@ -99,7 +100,10 @@ def main():
     quiet = args.quiet
 
     target_date = None
-    if args.date:
+    if args.tomorrow:
+        from datetime import timedelta
+        target_date = datetime.now() + timedelta(days=1)
+    elif args.date:
         try:
             target_date = datetime.strptime(args.date, "%Y-%m-%d")
         except ValueError:
